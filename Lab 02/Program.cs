b9d922fff2895c5f9e3d17feb833b981e3e37573
Daniel Tucker <TUCKERD1@ETSU.EDU>
2022-09-01 18:05:48 -0400

    part 1

--- Lab 02/Program.cs
+++ Lab 02/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Console.ReadLine is how we read a line of text
+using System.Xml.Linq;
+
+Console.WriteLine("Input first number to be added: ");
+var firstNumberAsString = Console.ReadLine();
+
+int firstNum;
+while (!int.TryParse(firstNumberAsString, out firstNum))
+{
+    Console.WriteLine("This is not a number!");
+    firstNumberAsString = Console.ReadLine();
+}
+// ask for second number
+Console.WriteLine("Input second number to be added: ");
+var secondNumberAsString = Console.ReadLine();
+
+int secondNum;
+while (!int.TryParse(secondNumberAsString, out secondNum))
+{
+    Console.WriteLine("This is not a number!");
+    secondNumberAsString = Console.ReadLine();
+}
+
+// add them and display the answer
+int sum = firstNum + secondNum;
+Console.WriteLine("The sum of " + firstNum + " and " + secondNum + " is: " + sum);
